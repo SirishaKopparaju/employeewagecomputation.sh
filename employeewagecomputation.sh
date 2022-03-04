3fab9ae575810714f3e9bc17c4250f9d3da2fc14
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-04 10:30:15 +0530

    Added: to calculate wage if employee is present

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 IS_PRESENT=1;
+
 checkEmp=$((RANDOM%2))
 
 if [ $checkEmp -eq $IS_PRESENT ]
 then
+empHrs=8;
+EMP_RATE_PER_HRS=20;
+salary=$((empHrs*EMP_RATE_PER_HRS))
+echo "Salary=$salary"
 echo "employee is present"
 else
 echo "employee is absent"
