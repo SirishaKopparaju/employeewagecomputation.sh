729c464da216ad82ff88a5bc916ec57dbd10e165
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-04 10:51:54 +0530

    ADDED: to know if employee is fulltime or parttime

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
 IS_PRESENT=1;
-
+IS_PART=2;
 checkEmp=$((RANDOM%2))
 
 if [ $checkEmp -eq $IS_PRESENT ]
 then
 empHrs=8;
 EMP_RATE_PER_HRS=20;
-salary=$((empHrs*EMP_RATE_PER_HRS))
-echo "Salary=$salary"
+
 echo "employee is present"
 else
+empHrs=4;
 echo "employee is absent"
 fi
+salary=$((empHrs*EMP_RATE_PER_HRS))
+echo "Salary=$salary"
